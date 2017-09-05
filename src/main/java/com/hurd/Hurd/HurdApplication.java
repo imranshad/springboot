fed43cbe3723b815afa3e3eebdfb1840adaf2dfa
shad <shad@DESKTOP-R4TRA9N>
2017-09-05 15:05:02 +0500

    just uploaded still .............

---
+++ src/main/java/com/hurd/Hurd/HurdApplication.java
@@ -0,0 +1,12 @@
+package com.hurd.Hurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurdApplication.class, args);
+	}
+}
