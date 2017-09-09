3adf64b921c9ca2ad1b79ca8035aef36d76c24f4
imran shad <imran.shad>
2017-09-09 14:26:38 -0700

    removed extra mappings

--- src/main/java/com/hurd/Hurd/Controlers/Home.java
+++ src/main/java/com/hurd/Hurd/Controlers/Home.java
@@ -10,46 +10,10 @@ public class Home {
     @RequestMapping("/")
     public ModelAndView Index(ModelAndView model){
         model=new ModelAndView("index");
-    return model;
-    }
-        @RequestMapping("/home")
-    public ModelAndView Home(ModelAndView model){
-        model=new ModelAndView("/pages/home");
-    return model;
-    }
-            @RequestMapping("/about")
-    public ModelAndView About(ModelAndView model){
-        model=new ModelAndView("/pages/about");
-    return model;
-    }
-            @RequestMapping("/one")
-    public ModelAndView One(ModelAndView model){
-        model=new ModelAndView("/pages/one");
-    return model;
-    }
-            @RequestMapping("/contact")
-    public ModelAndView Two(ModelAndView model){
-        model=new ModelAndView("/pages/contact");
-    return model;
-    }
-    @RequestMapping("/three")
-    public ModelAndView Three(ModelAndView model){
-        model=new ModelAndView("/pages/three");
-    return model;
-    }
-    @RequestMapping("/four")
-    public ModelAndView Four(ModelAndView model){
-        model=new ModelAndView("/pages/four");
-    return model;
-    }
-    @RequestMapping("five")
-    public ModelAndView Five(ModelAndView model){
-        model=new ModelAndView("/pages/five");
-    return model;
-    }
-    @RequestMapping("")
-    public ModelAndView ContactUs(ModelAndView model){
-        model=new ModelAndView("/pages/contactus");
+    model.addObject("msg","hello pakistan");
         return model;
+
     }
+
+
 }
--- src/main/java/com/hurd/Hurd/HurdApplication.java
+++ src/main/java/com/hurd/Hurd/HurdApplication.java
@@ -1,5 +1,6 @@
 package com.hurd.Hurd;
 
+import com.hurd.Hurd.Controlers.Home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,7 @@ public class HurdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HurdApplication.class, args);
+
 	}
+
 }
